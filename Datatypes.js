b9c4eded6a01a162afbd80b4c940fe677bb2d911
Avinash Mishra <149099980+avinashmishra002@users.noreply.github.com>
2024-05-27 17:50:30 +0530

    Add files via upload

---
+++ Datatypes.js
@@ -0,0 +1,44 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+ 4)  //we are uasing nodejs, not browser
+
+
+// JavaScript has 8 Datatypes
+// String
+// Number
+// Bigint
+// Boolean
+// Undefined  
+// Null  ==> standlone value
+// Symbol ==>&,$,!
+// Object
+
+
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");
+
+console.log(typeof Date);
+
+
+console.log(typeof undefined); //undedined
+console.log(typeof null);  // object
+
---
+++ Variebals.js
@@ -0,0 +1,25 @@
+const accountId =12345
+let accountEmail ="Avinash0@gmail.com"
+var accountpassword ="54321"
+
+accountCity ="delhi"
+
+let accountState;  // undifinde
+
+// acoountId = 2 //not allowed
+
+accountEmail ="ab@cf.com"
+accountpassword ="755425"
+accountCity = "up"
+
+
+
+// console.log(accountId);
+
+
+/* prefer not to use var because of issure in block scope and functionl scope
+*/
+
+console.table([accountId, accountEmail,accountpassword,accountCity])
+
+
